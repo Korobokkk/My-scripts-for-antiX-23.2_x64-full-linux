91e16b1b71acd226d060f916cd9aedbe7c14431f
Korobokkk <Igor227@LinuxUser228>
2026-01-16 16:00:22 +0300

    fixed display bag with scale

--- monitor_multy.sh
+++ monitor_multy.sh
@@ -7,6 +7,16 @@ STATE_FILE="$HOME/.monitor_state"
 LAPTOP="LVDS-1"
 HDMI="HDMI-1"
 
+#для фикса бага с свъездом раздельного экрана
+WALLPAPERHDMI="/home/Igor227/Изображения/HDMI_PAINT.png"
+WALLPAPERLAPTOP="/home/Igor227/Изображения/LAPTOP_PAINT.png"
+
+set_wallpaper_2_display(){
+    xwallpaper --output "$LAPTOP" --stretch "$WALLPAPERLAPTOP" \
+           --output "$HDMI" --stretch "$WALLPAPERHDMI"
+}
+
+
 # если файла состояния нет — создаём
 [ ! -f "$STATE_FILE" ] && echo 0 > "$STATE_FILE"
 
@@ -16,6 +26,7 @@ STATE=$(cat "$STATE_FILE")
 if ! xrandr | grep -q "$HDMI connected"; then
     xrandr --output "$HDMI" --off --output "$LAPTOP" --auto --primary
     echo 0 > "$STATE_FILE"
+    xwallpaper --output "$LAPTOP" --stretch "$WALLPAPERLAPTOP"
     exit 0
 fi
 
@@ -25,17 +36,20 @@ case "$STATE" in
         xrandr --output "$HDMI" --auto --primary \
                --output "$LAPTOP" --off
         echo 1 > "$STATE_FILE"
+        xwallpaper --output "$HDMI" --stretch "$WALLPAPERHDMI"
         ;;
     1)
         # оба, HDMI слева
         xrandr --output "$HDMI" --auto --left-of "$LAPTOP" \
                --output "$LAPTOP" --auto --primary
         echo 2 > "$STATE_FILE"
+        set_wallpaper_2_display
         ;;
     2)
         # оба, HDMI справа
         xrandr --output "$HDMI" --auto --right-of "$LAPTOP" \
                --output "$LAPTOP" --auto --primary
         echo 0 > "$STATE_FILE"
+        set_wallpaper_2_display
         ;;
 esac
