a6201bbdffe8a4865b4acf13d536d2854e526d73
Korobokkk <Igor227@LinuxUser228>
2026-01-17 20:29:23 +0300

    Set HDMI as primary in dual-screen mode and add images directory

--- monitor_multy.sh
+++ monitor_multy.sh
@@ -8,19 +8,20 @@ LAPTOP="LVDS-1"
 HDMI="HDMI-1"
 
 #для фикса бага с свъездом раздельного экрана
-WALLPAPERHDMI="/home/Igor227/Изображения/HDMI_PAINT.png"
-WALLPAPERLAPTOP="/home/Igor227/Изображения/LAPTOP_PAINT.png"
+WALLPAPERHDMI="/home/Igor227/Изображения/DisplayImage2HDMI.png"
+WALLPAPERLAPTOP="/home/Igor227/scripts/My-scripts-for-antiX-23.2_x64-full-linux/Display_Image/LAPTOP_PAINT.png"
 
 set_wallpaper_2_display(){
     xwallpaper --output "$LAPTOP" --stretch "$WALLPAPERLAPTOP" \
            --output "$HDMI" --stretch "$WALLPAPERHDMI"
 }
 
-
 # если файла состояния нет — создаём
 [ ! -f "$STATE_FILE" ] && echo 0 > "$STATE_FILE"
 
 STATE=$(cat "$STATE_FILE")
+
+#xrandr -fb 1366x768 #костыль, через пару циклов можно сетнуть
 
 # если HDMI не подключён — всегда ноутбук
 if ! xrandr | grep -q "$HDMI connected"; then
@@ -29,6 +30,9 @@ if ! xrandr | grep -q "$HDMI connected"; then
     xwallpaper --output "$LAPTOP" --stretch "$WALLPAPERLAPTOP"
     exit 0
 fi
+
+xrandr --output "$HDMI" --off \
+       --output "$LAPTOP" --off
 
 case "$STATE" in
     0)
@@ -40,15 +44,15 @@ case "$STATE" in
         ;;
     1)
         # оба, HDMI слева
-        xrandr --output "$HDMI" --auto --left-of "$LAPTOP" \
-               --output "$LAPTOP" --auto --primary
+        xrandr --output "$HDMI" --auto --left-of "$LAPTOP" --primary \
+               --output "$LAPTOP" --auto 
         echo 2 > "$STATE_FILE"
         set_wallpaper_2_display
         ;;
     2)
         # оба, HDMI справа
-        xrandr --output "$HDMI" --auto --right-of "$LAPTOP" \
-               --output "$LAPTOP" --auto --primary
+        xrandr --output "$HDMI" --auto --right-of "$LAPTOP" --primary \
+               --output "$LAPTOP" --auto 
         echo 0 > "$STATE_FILE"
         set_wallpaper_2_display
         ;;
